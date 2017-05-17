8643fa1eed34ce434d7bce5927ae49bea39f2c39
Monica Howard <jay.m.howard@gmail.com>
2017-05-17 12:18:28 -0500

    working pretty damn good

--- script.js
+++ script.js
@@ -35,13 +35,14 @@ leftQuery.addEventListener('keyup', function(evt) {
       url: "https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=" + movieTitle
     });
     promise.done(function(data) {
-     console.log("First Movie Title", data.results[0].title);
-     console.log("First Movie Rating", data.results[0].vote_average);
+    //  console.log("First Movie Title", data.results[0].title);
+    //  console.log("First Movie Rating", data.results[0].vote_average);
      movie1Title = data.results[0].title;
      movie1Average = data.results[0].vote_average;
     });
-console.log("whats the freaking average", movie1Average);
-console.log("whats the freaking average", movie1Title);
+    console.log("Movie Name:", movie1Title);
+    console.log("Movie Average:", movie1Average);
+
 
 });
 
@@ -67,10 +68,26 @@ rightQuery.addEventListener('keyup', function(evt) {
       url: "https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=" + movieTitle
     });
     promise.done(function(data) {
-     console.log("Second Movie Title", data.results[0].title);
-     console.log("Second Movie Rating", data.results[0].vote_average);
+    //  console.log("Second Movie Title", data.results[0].title);
+    //  console.log("Second Movie Rating", data.results[0].vote_average);
+     movie2Title = data.results[0].title;
+     movie2Average = data.results[0].vote_average;
     });
+    console.log("Movie Name:", movie2Title);
+    console.log("Movie Average:", movie2Average);
+});
 
 
+var duelButton = document.querySelector("#duel-button");
+duelButton.addEventListener('click', function() {
 
+    if (movie1Average > movie2Average) {
+      return alert('THE WINNER IS: ' + movie1Title);
+    }
+      else if {
+        return alert('THE WINNER IS: ' + movie2Title);
+      }
+        else {
+          alert('DRAW');
+        }
 });
