8c58e951a79573f9af41a5c43e4484bfcb582d22
Monica Howard <jay.m.howard@gmail.com>
2017-05-17 10:44:54 -0500

    decent stopping place needs better config for rating and title

--- script.js
+++ script.js
@@ -8,16 +8,17 @@ function makeAjaxCall(movieTitle){
      });
      promise.done(function(data) {
       console.log(data);
-      console.log("hooooooo", data.results[0].title);
-      console.log("fooooooo", data.results[0].vote_average);
+    //   console.log("Movie Title", data.results[0].title);
+    //   console.log("fooooooo", data.results[0].vote_average);
      });
  }
 
-
+var movie1Title;
+var movie1Average;
 var leftQuery = document.querySelector('.left-query');
 leftQuery.addEventListener('keyup', function(evt) {
   if (evt.keyup !== 13) {
-  var movie1Title = leftQuery.value;
+  movie1Title = leftQuery.value;
   }
   console.log("what?", movie1Title);
   var re = / /gi;
@@ -29,16 +30,24 @@ leftQuery.addEventListener('keyup', function(evt) {
   makeAjaxCall(movieTitle);
   console.log("check", movieTitle);
 
-
+  var promise =
+    $.ajax ({
+      url: "https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=" + movieTitle
+    });
+    promise.done(function(data) {
+     console.log("First Movie Title", data.results[0].title);
+     console.log("First Movie Rating", data.results[0].vote_average);
+     movie1Title = data.results[0].title;
+     movie1Average = data.results[0].vote_average;
+    });
+console.log("whats the freaking average", movie1Average);
+console.log("whats the freaking average", movie1Title);
 
 });
 
 
-
-
-
-
-
+var movie2Title;
+var movie2Average;
 var rightQuery = document.querySelector('.right-query');
 rightQuery.addEventListener('keyup', function(evt) {
   (evt.keyCode !== 13)
@@ -47,4 +56,21 @@ rightQuery.addEventListener('keyup', function(evt) {
   var re = / /gi;
   movieTwo = movie2Title.replace(re, "+")
   console.log("hey", movieTwo);
+
+  movieTitle = movieTwo;
+
+  makeAjaxCall(movieTitle);
+  console.log("check", movieTitle);
+
+  var promise =
+    $.ajax ({
+      url: "https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=" + movieTitle
+    });
+    promise.done(function(data) {
+     console.log("Second Movie Title", data.results[0].title);
+     console.log("Second Movie Rating", data.results[0].vote_average);
+    });
+
+
+
 });
