7c44750b313999501db9a9564b02aa0841259046
Monica Howard <jay.m.howard@gmail.com>
2017-05-16 22:20:09 -0500

    ajax and inputs working

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 var promise =
+makeAjaxCall();
 
-  $.ajax ({
-    url: "https://api.themoviedb.org/3/search/movie?api_key=ec457859cd32502859fced3c3ca8ede&query=Jack+Reacher"
 
-    //  'https://api.themoviedb.org/3/discover/movie?page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=dec457859cd32502859fced3c3ca8ede'
-  });
+function makeAjaxCall(){
+  var promise =
+    $.ajax ({
+      url: "https://api.themoviedb.org/3/search/movie?api_key=dec457859cd32502859fced3c3ca8ede&query=" + movieOne
+    });
+    promise.done(function(data) {
+      console.log(data);
+    });
+}
 
-  promise.done(function(data) {
-    console.log(data);
-  });
+
+var leftQuery = document.querySelector('.left-query');
+leftQuery.addEventListener('keyup', function(evt) {
+  (evt.keyCode !== 13)
+  var movie1Title = leftQuery.value;
+  console.log("what?", movie1Title);
+  var re = / /gi;
+  movieOne = movie1Title.replace(re, "+")
+  console.log("hey", movieOne);
+});
+
+
+
+var rightQuery = document.querySelector('.right-query');
+rightQuery.addEventListener('keyup', function(evt) {
+  (evt.keyCode !== 13)
+  var movie2Title = rightQuery.value;
+  console.log("what?", movie2Title);
+  var re = / /gi;
+  movieTwo = movie2Title.replace(re, "+")
+  console.log("hey", movieTwo);
+});
